64c68ea13ec1ec40197af6541b9e97897c6ca455
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2026-05-29 11:19:59 +0200

    Added TRexServer struct skeleton and imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rmcp::{
+    ErrorData as McpError, RoleServer, ServerHandler,
+    handler::server::{
+        router::{prompt::PromptRouter, tool::ToolRouter},
+        wrapper::Parameters,
+    },
+    model::*,
+    prompt, prompt_handler, prompt_router, schemars,
+    service::RequestContext,
+    task_handler,
+    task_manager::{OperationProcessor, OperationResultTransport},
+    tool, tool_handler, tool_router,
+};
+
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Clone)]
+pub struct TRexServer {
+    tool_router: ToolRouter<TRexServer>,
 }
