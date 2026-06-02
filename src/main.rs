5358203608831c4ffb294b67d692236785db07df
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2026-06-02 09:45:06 +0200

    Add main with stdio server, complete ping tool

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use rmcp::{
-    ErrorData as McpError, ServerHandler,
+    ErrorData as McpError, ServerHandler,ServiceExt,
     handler::server::router::tool::ToolRouter,
     model::*,
-    tool, tool_handler, tool_router,
+    tool, tool_handler, tool_router, transport::stdio,
 };
 
 
-
-fn main() {
-    //
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mcp_server = TRexServer::new().serve(stdio()).await?;
+    mcp_server.waiting().await?;
+    Ok(())
 }
 
 #[derive(Clone)]
@@ -28,5 +30,17 @@ impl TRexServer {
     #[tool(description = "Ping-pong check for client")]
     pub async fn ping(&self) -> Result<CallToolResult, McpError> {
         Ok(CallToolResult::success(vec![Content::text("pong")]))
+    }
+}
+
+#[tool_handler]
+impl ServerHandler for TRexServer {
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo::new(
+            ServerCapabilities::builder()
+                .enable_tools()
+                .build(),
+        )
+        .with_server_info(Implementation::from_build_env())
     }
 }
